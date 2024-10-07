604e87792d5234e74f2d38ca8375dd72531cf642
eresende-hsc <eder.resende@hospitalsaocamilosp.org.br>
2024-10-07 10:39:12 -0300

    upgrade memory size

--- winrm-enable.ps1
+++ winrm-enable.ps1
@@ -32,7 +32,7 @@ cmd.exe /c winrm quickconfig -q
 
 cmd.exe /c winrm set "winrm/config" '@{MaxTimeoutms="1800000"}'
 
-cmd.exe /c winrm set "winrm/config/winrs" '@{MaxMemoryPerShellMB="1024"}'
+cmd.exe /c winrm set "winrm/config/winrs" '@{MaxMemoryPerShellMB="32768"}'
 
 cmd.exe /c winrm set "winrm/config/service" '@{AllowUnencrypted="true"}'
 
